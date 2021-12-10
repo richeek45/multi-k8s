b92f9ccd19bc3295ebf90bd8cc6dd7486fe44f8b
Richeek45 <richeek45@gmail.com>
2021-12-10 13:42:50 +0530

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const OtherPage = () => {
+  return (
+    <>
+      Is some other Page?
+      <Link to="/">Go back Home!</Link>
+    </>
+  );
+};
+
+export default OtherPage;
